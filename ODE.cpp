60b811d5e7d5ffb4ea5dda4477f30d5a6d588a23
Juan Pablo Acuña Acosta <UANDES\jp.acuna@fisicaez12204l.ad.uniandes.edu.co>
2018-11-19 15:18:31 -0500

    Revision a meotod

--- ODE.cpp
+++ ODE.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 void paso(float theta);
 
-float m=200;
+float m=0.2;
 float c=0.2;
 float g=10;
 float v=300;
@@ -19,7 +19,7 @@ float x[500];
 float y[500];
 int main()
 {
-	paso(45.);
+	paso(45.*3.1425962/180.);
 	return 0;
 }
 
@@ -34,20 +34,18 @@ void paso(float theta)
 	float t=0;
 	
 	
-	float deltat = 0.01;
-	vx[0]=v*cos(theta);
-	vy[0]=v*sin(theta);
-	vx[1]=vx[0]+deltat*-k*sqrt(vx[0]*vx[0]+vy[0]*vy[0])*vx[0];
-	vy[1]=vy[0]+deltat*-k*sqrt(vx[0]*vx[0]+vy[0]*vy[0])*vy[0];
+	float deltat = 0.001;
+	vx[0]=v*cos(theta)-deltat*(-v*cos(theta)*k*sqrt(v*sin(theta)*v*sin(theta)+v*cos(theta)*v*cos(theta)))/2.;
+	vy[0]=v*sin(theta)-deltat*(-10.-v*sin(theta)*k*sqrt(v*sin(theta)*v*sin(theta)+v*cos(theta)*v*cos(theta)))/2.;
+	
 	x[0]=0.;
 	y[0]=0.;
-	x[1]=x[0]+deltat*vx[0];
-	y[1]=y[0]+deltat*vy[0];
+	
 
 	
 	
 
-	for(int i=2; i<500; i++)
+	for(int i=1; i<500; i++)
 	{
 		t= t+deltat;
 		vy[i]= vy[i-2]+2*deltat*(-10.-vy[i-1]*k*sqrt((vy[i-1]*vy[i-1])+(vx[i-1]*vx[i-1])));
